ade4ab44ad8c16a57c4762bc3fbd26b4306557e5
Jonas Wiesinger <jonas.walter.wiesinger@gmail.com>
2020-10-27 11:41:03 +0100

    add gallery

--- script/images.js
+++ script/images.js
@@ -1,11 +1,24 @@
-let amountImg = 13;
+var imgLinks = [
+    "https://www.instagram.com/jowies_photography/?hl=de",
+    "https://www.instagram.com/p/BtYIfVmHXby/?utm_source=ig_web_copy_link",
+    "https://www.instagram.com/p/BqPoxkaBkCz/?utm_source=ig_web_copy_link",
+    "https://www.instagram.com/jowies_photography/?hl=de",
+    "https://www.instagram.com/p/BqC9dhhhpTx/?utm_source=ig_web_button_share_sheet",
+    "https://www.instagram.com/p/B0qfaIvDpKA/?utm_source=ig_web_copy_link",
+    "https://www.instagram.com/p/BqxedEaBuUg/?utm_source=ig_web_button_share_sheet",
+    "https://bach-chor.com/medien/#post-216",
+    "https://bach-chor.com/medien/#post-216",
+    "https://bach-chor.com/medien/#post-216",
+    "https://www.instagram.com/jowies_photography/?hl=de",
+    "https://www.instagram.com/jowies_photography/?hl=de",
+    "https://www.instagram.com/jowies_photography/?hl=de"];
 
 window.addEventListener('load', () => {
     let imgContainer = document.getElementById('imgContainer');
 
-    for (let i = amountImg; i >= 1; i--) {
+    for (let i = imgLinks.length; i >= 1; i--) {
         let img = document.createElement('img');
-        img.setAttribute('src', './media/photos/' + i + '.jpg');
+        img.setAttribute('src', './media/photos/small/' + i + '.jpg');
 
         imgContainer.appendChild(img);
     }
