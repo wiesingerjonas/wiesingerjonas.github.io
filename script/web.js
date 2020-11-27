f800ab66c83eb6402c342488911f56386ab4e3b0
Jonas Wiesinger <jonas.walter.wiesinger@gmail.com>
2020-11-27 16:09:17 +0100

    add img to vshart

--- script/web.js
+++ script/web.js
@@ -2,7 +2,7 @@ window.addEventListener('load', () => {
   const web =
     '{"web":[' +
     '{"title":"HTBLA Leonie Landing Page", "iframe":"null", "pic":"landingpage.jpg", "text":"One of our most important subjects at school is project development. I am member of a project team, in which we are creating a voice assistant. Right now, my task is to renew the landing page of our Project, The Landing Page should give visitors general and technical information about our work."},' +
-    '{"title":"Vs Hart", "iframe":"null", "pic":"null", "text":"I am currently working on a web project with two classmembers. We are renewing the Webpage of the Vs Hart. Unfortunately, the project is delayed due to the global pandemic and the school closure."},' +
+    '{"title":"Vs Hart", "iframe":"null", "pic":"vshart.jpg", "text":"I am currently working on a web project with two classmembers. We are renewing the Webpage of the Vs Hart. Unfortunately, the project is delayed due to the global pandemic and the school closure."},' +
     '{"title":"Theatermonitor", "iframe":"https://www.youtube.com/embed/4AfV2YXBmrg", "pic":"null", "text":"I\'ve worked on this project for almost one year. It\'s a monitor, which theaters can use to give the audience information about the current play. In my case it is for the Musiktheater Linz."},' +
     '{"title":"Htl Dir Av", "iframe":"https://htldirav.net/", "pic":"null", "text":"null"},' +
     '{"title":"Adventskalender", "iframe":"https://wiesingerjonas.github.io/adventskalender/", "pic":"null", "text":"null"},' +
