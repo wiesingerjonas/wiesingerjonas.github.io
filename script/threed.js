0cf86baae499ccee21522fbe44e09d3a5ef9e364
Jonas Wiesinger <jonas.walter.wiesinger@gmail.com>
2020-12-05 14:34:47 +0100

    add toggle mute button

---
+++ script/threed.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+    let buttonSoft = document.getElementById('buttonSoft');
+    let softVideo = document.getElementById('softVideo');
+
+    softVideo.addEventListener('click', () => {
+        console.log("zeas");
+        softVideo.muted = !softVideo.muted;
+
+        if (buttonSoft.classList.length === 0){
+            buttonSoft.classList.add("muted");
+        } else {
+            buttonSoft.className = "";
+        }
+    });
+});
